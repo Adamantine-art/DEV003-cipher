68ccb25028519e31cd044310d7d20f0720e3d4f2
Adamantine-art <andreita.mosqueira@gmail.com>
2022-12-12 17:37:41 -0300

    Proyecto Terminado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,70 @@
 const cipher = {
-  // ...
+
+  encode(textoNuevo, numeroCifrado) {
+    let textoCodificado = '';
+
+    const respuestaEnMayus = textoNuevo.toUpperCase();
+
+    for (let index = 0; index < textoNuevo.length; index++) {
+
+      const codigoLetraOriginal = respuestaEnMayus.charCodeAt(index);
+
+      if (codigoLetraOriginal === 32) {
+        textoCodificado += respuestaEnMayus[index]
+        continue 
+      }
+
+      const posicion = (codigoLetraOriginal - 65 + numeroCifrado) % 26
+
+      let codigoDeLetraCifrada
+      if (posicion < 0) {
+        codigoDeLetraCifrada = 91 - posicion
+
+      } else {
+        codigoDeLetraCifrada = posicion + 65
+      }
+
+      textoCodificado += String.fromCharCode(codigoDeLetraCifrada);
+
+    }
+
+    return textoCodificado;
+
+  },
+
+  decode(textoRecibido, numeroCifrado) {
+
+    let textoDecodificado = '';
+
+    const respuestaEnMayus = textoRecibido.toUpperCase();
+
+    for (let index = 0; index < textoRecibido.length; index++) {
+
+      const codigoLetraConvertida = respuestaEnMayus.charCodeAt(index);
+
+      if (codigoLetraConvertida === 32) {
+        textoDecodificado += respuestaEnMayus[index]
+        continue 
+      }
+
+      const posicion = (codigoLetraConvertida - 65 - numeroCifrado) % 26
+
+      let codigoDeLetraDescifrada
+      if (posicion < 0) {
+        codigoDeLetraDescifrada = 91 + posicion
+
+      } else {
+        codigoDeLetraDescifrada = posicion + 65
+      }
+
+      textoDecodificado += String.fromCharCode(codigoDeLetraDescifrada);
+
+    }
+
+    return textoDecodificado;
+
+  }
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const cajaDeTexto = document.getElementById("texto");
+const botonCifrar = document.getElementById("codeButton");
+const botonDescifrar = document.getElementById("decodeButton");
+const cajaDeNumero = document.getElementById("key");
+
+botonCifrar.addEventListener("click", () => {
+    
+  const input = cajaDeTexto.value;
+  const offset = parseInt(cajaDeNumero.value);
+
+  cajaDeTexto.value = cipher.encode(input, offset);
+
+});
+
+botonDescifrar.addEventListener("click", () => {
+    
+  const input = cajaDeTexto.value;
+  const offset = parseInt(cajaDeNumero.value);
+  
+  cajaDeTexto.value = cipher.decode(input, offset);
+  
+});
